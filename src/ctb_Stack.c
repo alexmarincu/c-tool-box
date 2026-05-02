2db311d154facc581718cc16fef808020ae1b3f8
Alex Marincu <alex.marincu@gmail.com>
2026-05-02 22:00:38 +0300

    feat(ctb): implement Stack with push/pop/peek operations and unit tests

--- src/ctb_Stack.c
+++ src/ctb_Stack.c
@@ -1,1 +1,52 @@
 #include "ctb_Stack.h"
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+ctb_Stack_t * ctb_Stack_init(
+    ctb_Stack_t * const self
+) {
+    ctb_SList_init(&self->list);
+
+    return self;
+}
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+void ctb_Stack_push(
+    ctb_Stack_t * const self,
+    ctb_SNode_t * const node
+) {
+    ctb_SList_addFirst(&self->list, node);
+}
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+ctb_SNode_t * ctb_Stack_pop(
+    ctb_Stack_t * const self
+) {
+    ctb_SNode_t * node = NULL;
+
+    if (!ctb_Stack_isEmpty(self)) {
+        node = ctb_SList_removeFirst(&self->list);
+    }
+
+    return node;
+}
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+ctb_SNode_t * ctb_Stack_peek(
+    ctb_Stack_t const * const self
+) {
+    return ctb_SList_getFirst(&self->list);
+}
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+bool ctb_Stack_isEmpty(
+    ctb_Stack_t const * const self
+) {
+    return ctb_SList_isEmpty(&self->list);
+}
+
+// cppcheck-suppress [staticFunction, unusedFunction] - API function
+void ctb_Stack_clear(
+    ctb_Stack_t * const self
+) {
+    ctb_SList_clear(&self->list);
+}
---
+++ test/test_ctb_Stack.c
@@ -0,0 +1,75 @@
+#include "unity.h"
+
+#include "ctb_Stack.h"
+
+#include "ctb_SList.h"
+#include "ctb_SNode.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_ctb_Stack_init_should_initialize_stack(void) {
+    ctb_Stack_t stack;
+    ctb_Stack_init(&stack);
+    TEST_ASSERT_TRUE(ctb_Stack_isEmpty(&stack));
+    TEST_ASSERT_NULL(ctb_Stack_peek(&stack));
+}
+
+void test_ctb_Stack_push_should_add_node_to_top(void) {
+    ctb_Stack_t stack;
+    ctb_Stack_init(&stack);
+    ctb_SNode_t node1, node2;
+    ctb_SNode_init(&node1);
+    ctb_SNode_init(&node2);
+
+    ctb_Stack_push(&stack, &node1);
+    TEST_ASSERT_FALSE(ctb_Stack_isEmpty(&stack));
+    TEST_ASSERT_EQUAL_PTR(&node1, ctb_Stack_peek(&stack));
+
+    ctb_Stack_push(&stack, &node2);
+    TEST_ASSERT_EQUAL_PTR(&node2, ctb_Stack_peek(&stack));
+}
+
+void test_ctb_Stack_pop_should_remove_node_from_top(void) {
+    ctb_Stack_t stack;
+    ctb_Stack_init(&stack);
+    ctb_SNode_t node1, node2;
+    ctb_SNode_init(&node1);
+    ctb_SNode_init(&node2);
+
+    ctb_Stack_push(&stack, &node1);
+    ctb_Stack_push(&stack, &node2);
+
+    TEST_ASSERT_EQUAL_PTR(&node2, ctb_Stack_pop(&stack));
+    TEST_ASSERT_EQUAL_PTR(&node1, ctb_Stack_peek(&stack));
+
+    TEST_ASSERT_EQUAL_PTR(&node1, ctb_Stack_pop(&stack));
+    TEST_ASSERT_TRUE(ctb_Stack_isEmpty(&stack));
+    TEST_ASSERT_NULL(ctb_Stack_pop(&stack));
+}
+
+void test_ctb_Stack_peek_should_return_top_node_without_removing(void) {
+    ctb_Stack_t stack;
+    ctb_Stack_init(&stack);
+    ctb_SNode_t node1;
+    ctb_SNode_init(&node1);
+
+    ctb_Stack_push(&stack, &node1);
+    TEST_ASSERT_EQUAL_PTR(&node1, ctb_Stack_peek(&stack));
+    TEST_ASSERT_FALSE(ctb_Stack_isEmpty(&stack));
+}
+
+void test_ctb_Stack_clear_should_empty_stack(void) {
+    ctb_Stack_t stack;
+    ctb_Stack_init(&stack);
+    ctb_SNode_t node1, node2;
+    ctb_SNode_init(&node1);
+    ctb_SNode_init(&node2);
+
+    ctb_Stack_push(&stack, &node1);
+    ctb_Stack_push(&stack, &node2);
+
+    ctb_Stack_clear(&stack);
+    TEST_ASSERT_TRUE(ctb_Stack_isEmpty(&stack));
+    TEST_ASSERT_NULL(ctb_Stack_peek(&stack));
+}
