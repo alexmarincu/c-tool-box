5152f3e8d0898292e306e1a6c03dcbe583ca4013
Alex Marincu <alex.marincu@gmail.com>
2026-06-14 21:32:31 +0300

    refactor(ctb): rename ctb_Stack and ctb_Queue to ctb_StackList and ctb_QueueList
    Rename list-backed Stack and Queue types to StackList and QueueList to
    align with the ADT x Storage naming convention (e.g., ctb_StackArray,
    ctb_StackList, ctb_QueueArray, ctb_QueueList).
    
    - Rename src/ctb_Stack.h -> src/ctb_StackList.h and update symbols
    - Rename src/ctb_Stack.c -> src/ctb_StackList.c and update symbols
    - Rename src/ctb_Queue.h -> src/ctb_QueueList.h and update symbols
    - Rename src/ctb_Queue.c -> src/ctb_QueueList.c and update symbols
    - Rename test/test_ctb_Stack.c -> test/test_ctb_StackList.c
    - Rename test/test_ctb_Queue.c -> test/test_ctb_QueueList.c
    - Update include guards: CTB_STACK_H -> CTB_STACKLIST_H,
      CTB_QUEUE_H -> CTB_QUEUELIST_H
    - All 71 tests pass
    
    Closes #6

---
+++ test/test_ctb_QueueList.c
@@ -0,0 +1,64 @@
+#include "unity.h"
+
+#include "ctb_QueueList.h"
+
+#include "ctb_DList.h"
+#include "ctb_DListIterator.h"
+#include "ctb_DNode.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_ctb_QueueList_init_should_initialize_queue(void) {
+    ctb_QueueList_t queue;
+    ctb_QueueList_init(&queue);
+    TEST_ASSERT_TRUE(ctb_QueueList_isEmpty(&queue));
+}
+
+void test_ctb_QueueList_enqueue_should_add_node_to_queue(void) {
+    ctb_QueueList_t queue;
+    ctb_QueueList_init(&queue);
+    ctb_DNode_t node1, node2;
+    ctb_DNode_init(&node1);
+    ctb_DNode_init(&node2);
+
+    ctb_QueueList_enqueue(&queue, &node1);
+    TEST_ASSERT_FALSE(ctb_QueueList_isEmpty(&queue));
+
+    ctb_QueueList_enqueue(&queue, &node2);
+    TEST_ASSERT_FALSE(ctb_QueueList_isEmpty(&queue));
+}
+
+void test_ctb_QueueList_dequeue_should_remove_node_from_queue_in_FIFO_order(void) {
+    ctb_QueueList_t queue;
+    ctb_QueueList_init(&queue);
+    ctb_DNode_t node1, node2;
+    ctb_DNode_init(&node1);
+    ctb_DNode_init(&node2);
+
+    ctb_QueueList_enqueue(&queue, &node1);
+    ctb_QueueList_enqueue(&queue, &node2);
+
+    TEST_ASSERT_EQUAL_PTR(&node1, ctb_QueueList_dequeue(&queue));
+    TEST_ASSERT_FALSE(ctb_QueueList_isEmpty(&queue));
+
+    TEST_ASSERT_EQUAL_PTR(&node2, ctb_QueueList_dequeue(&queue));
+    TEST_ASSERT_TRUE(ctb_QueueList_isEmpty(&queue));
+
+    TEST_ASSERT_NULL(ctb_QueueList_dequeue(&queue));
+}
+
+void test_ctb_QueueList_clear_should_empty_queue(void) {
+    ctb_QueueList_t queue;
+    ctb_QueueList_init(&queue);
+    ctb_DNode_t node1, node2;
+    ctb_DNode_init(&node1);
+    ctb_DNode_init(&node2);
+
+    ctb_QueueList_enqueue(&queue, &node1);
+    ctb_QueueList_enqueue(&queue, &node2);
+
+    ctb_QueueList_clear(&queue);
+    TEST_ASSERT_TRUE(ctb_QueueList_isEmpty(&queue));
+    TEST_ASSERT_NULL(ctb_QueueList_dequeue(&queue));
+}
